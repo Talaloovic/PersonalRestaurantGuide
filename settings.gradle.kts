263fc3fcaef6ca40ac7700e7a0f25fbbcdcd8a71
Ibazone <159165981+Ibazone@users.noreply.github.com>
2024-12-11 22:21:06 -0500

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "PersonalRestaurantGuide"
 include(":app")
- 
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
